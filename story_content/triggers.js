7a0eb0d3dbd8dd350a47aa5097fc7a287d58dcbd
Aerohelal <103382858+Aerohelal@users.noreply.github.com>
2024-09-22 00:32:53 +0300

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,67 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5dXZOoEw4LF":
+        Script1();
+        break;
+      case "5br63zSmsWQ":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('623sSS8A7MU');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6XrxeiTmlFy';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('68szGhE2QAG');
+const duration = 750;
+const easing = 'ease-out';
+const id = '63OtMzvrz6X';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
